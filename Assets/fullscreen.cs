9861d830b2c3db8860856c9a89a98123889d356f
runluo <68489000+run-qiao@users.noreply.github.com>
2022-05-19 10:25:13 +0800

    Add files via upload
    add the fullscreen scripts

---
+++ Assets/fullscreen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class fullscreen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  按ESC退出全屏
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Screen.fullScreen = false;  //退出全屏         
+
+        }
+
+        //按A全屏
+        if (Input.GetKey(KeyCode.A))
+        {
+            Screen.SetResolution(1600, 900, true);
+
+            Screen.fullScreen = true;  //设置成全屏,
+        }
+    }
+}
+
+
